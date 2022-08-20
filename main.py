122c304d17a4d000ab367161d1fb2073c707bf7b
hungnt <hungnguyenthac@vccorp.vn>
2022-08-20 08:53:05 +0700

    reup absinthe1

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def Search():
     num_file = 0
     num_file_str = "%s" % num_file
     # fname = "tonic" + num_file_str
-    fname = "absinthe0"
+    fname = "absinthe1"
     ara = []         
     if os.path.isfile(fname):
         # File exists
